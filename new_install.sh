b081bab2a226cd127ceacda1dece9011da3c8845
EtienneDosSantos <130935112+EtienneDosSantos@users.noreply.github.com>
2024-03-16 16:54:47 +0100

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -10,7 +10,33 @@ if [ ! -d "stable-cascade-one-click-installer" ]; then
     git clone https://github.com/EtienneDosSantos/stable-cascade-one-click-installer
 else
     echo "Updating project..."
-    git pull
+    cd stable-cascade-one-click-installer && git pull
 fi
 
-echo "Done!"
+# Ensure we are in the stable-cascade-one-click-installer directory for Python environment setup
+cd "$script_dir/stable-cascade-one-click-installer" || exit
+
+# Set error flag
+set -e
+
+# Check for Python and exit if not found
+if ! [ -x "$(command -v python)" ]; then
+  echo 'Error: python is not installed.' >&2
+  exit 1
+fi
+
+# Create a virtual environment
+if [ ! -d "venv" ]; then
+  python -m venv venv
+fi
+
+# Upgrade pip before `pip install`
+./venv/bin/python -m pip install --upgrade pip
+
+# Install other requirements
+./venv/bin/pip install -r requirements.txt
+
+# Move new_install.sh into the root of stable-cascade-one-click-installer directory
+mv "$script_dir/new_install.sh" "$script_dir/stable-cascade-one-click-installer/"
+
+echo "Installation completed. Execute \`run.sh\` script to start generating!"
