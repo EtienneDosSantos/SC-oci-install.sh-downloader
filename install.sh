46040d0028b30468a7f6827ce9896d422b17cb90
EtienneDosSantos <130935112+EtienneDosSantos@users.noreply.github.com>
2024-03-16 19:36:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -36,7 +36,8 @@ fi
 # Install other requirements
 ./venv/bin/pip install -r requirements.txt
 
-# Move new_install.sh into the root of stable-cascade-one-click-installer directory
-mv "$script_dir/install.sh" "$script_dir/stable-cascade-one-click-installer/"
+echo "Installation completed. Execute 'run.sh' script to start generating!"
+echo "Press Enter to move 'install.sh' to the 'stable-cascade-one-click-installer' directory and conclude the installation."
+read -p "Press Enter to continue..."
 
-echo "Installation completed. Execute \`run.sh\` script to start generating!"
+mv "$script_dir/install.sh" "$script_dir/stable-cascade-one-click-installer/"
